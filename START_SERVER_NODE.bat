be345752c3660dff1f200c24d24a2da4dff1dcad
davistres <140393839+davistres@users.noreply.github.com>
2025-10-19 10:26:42 +0200

    finalisation du portefolio (avec la présentation des projet et le choix des photos)

---
+++ START_SERVER_NODE.bat
@@ -0,0 +1,8 @@
+@echo off
+echo ========================================
+echo   Demarrage du serveur Node.js
+echo ========================================
+echo.
+echo Verification de l'installation de http-server...
+call npx http-server -p 8000 -o
+
